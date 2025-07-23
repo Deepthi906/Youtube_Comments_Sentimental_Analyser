245a90ce61c438e7b0e8f3ebcb93695c22424e3b
Deepthi <vdeepthi906@gmail.com>
2025-07-24 01:30:07 +0530

    Add files via upload

---
+++ sentiment_analysis.py
@@ -0,0 +1,11 @@
+# sentiment_analysis.py
+from textblob import TextBlob
+
+def analyze_sentiment(text):
+    polarity = TextBlob(text).sentiment.polarity
+    if polarity > 0:
+        return "Positive"
+    elif polarity < 0:
+        return "Negative"
+    else:
+        return "Neutral"
